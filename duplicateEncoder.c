b7610a4da60f3a3504effb430600bc0aa3d501b6
trmarvel <karinabarinova11@gmail.com>
2020-08-20 00:01:36 +0300

    All tests passed

---
+++ duplicateEncoder.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+char *DuplicateEncoder(char *s) {
+	int len = strlen(s);
+	char *duplicate = NULL;
+	bool isDuplicate = false;
+	int i = 0;
+	duplicate = (char *)malloc(sizeof(char) * (len + 1));
+  
+	while (i <= len) {
+		duplicate[i] = '\0';
+    	i++;
+  	}
+
+
+	for (i = 0; i < len; i++) { 
+    	for (int j = 0; j < len; j++) {
+        	if (j == i) {
+            	continue;
+        	}
+      		if (tolower(s[i]) == tolower(s[j])) {
+        	duplicate[i] = ')';
+        	isDuplicate = true;
+        	break;
+      		} 
+    	}
+    	if (isDuplicate) {
+        	isDuplicate = false;
+        	continue;  
+    	}
+    	else {
+        	duplicate[i] = '(';
+    	}
+  	}
+	return duplicate;
+}
